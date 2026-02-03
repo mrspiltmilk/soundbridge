3dfb38c972b5c42d09dd4253495a2d818f1d4545
mrspiltmilk <johnny@futurefriendly.studio>
2026-02-03 19:47:15 +1100

    Removed stop from footer

--- App.tsx
+++ App.tsx
@@ -123,7 +123,7 @@ const App: React.FC = () => {
       </div>
 
       <footer className="fixed bottom-8 text-[9px] uppercase tracking-[0.4em] text-slate-400 dark:text-neutral-600 pointer-events-none text-center px-4">
-        Made with ♥ for music lovers.
+        Made with ♥ for music lovers
       </footer>
     </div>
   );
